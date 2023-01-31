52d8e7451056f27080bc2f4a97bd636937f2a192
giuliaa3 <124038808+giuliaa3@users.noreply.github.com>
2023-01-31 16:12:01 +0100

    Add files via upload

---
+++ numeri pari decrescenti da 100 a 0.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    numero= 100;
+    while (numero>=0 )
+    { 
+    printf("%d\n", numero);
+    numero= numero-2;
+}
+    return 0;
+}
