76585e82e8560c2d6098f02d0773edf1ac46dfef
giuliaa3 <124038808+giuliaa3@users.noreply.github.com>
2023-01-31 10:23:47 +0100

    Add files via upload

---
+++ a quanti anni si può prendere la patente.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{ 
+    int eta;
+
+    printf("a quanti anni si può prendere la patente");
+    scanf ("%d", &eta);
+    
+   
+  
+int anni;
+printf ("quanti anni hai");
+scanf("%d", &anni);
+if(eta<=anni)
+{ 
+printf ("è legale prenderla");
+    
+}
+else
+{ 
+printf ("non è legale prenderla");
+    
+}
+
+    return 0;
+}
