13cbe9671b0c5c4a782509ff15e8301555fbc02c
giuliaa3 <124038808+giuliaa3@users.noreply.github.com>
2023-02-01 16:58:39 +0100

    Add files via upload

---
+++ da finire.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void fattoriale(int numero) 
+
+
+
+int main()
+{
+    int numero;
+    int tot=1;
+    
+    printf("prendi un numero\n");
+    scanf("%d", &numero);
+    
+    while(n>0) {
+        tot=tpt/n
+        
+    }
+    
+    printf("finale%\n",tot);
+    
+    return 0;
+}
