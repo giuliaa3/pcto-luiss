178b30d2980c5ddf2dc800dfd2b876b937ba61cb
giuliaa3 <124038808+giuliaa3@users.noreply.github.com>
2023-02-01 12:49:51 +0100

    Add files via upload

---
+++ ft_ft(y).c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void ft_ft(int*nbr,int*x,int*y)
+{
+    *nbr=42;
+    *x=25;
+    y=102;
+    printf("il valore della funzione ft_ft(y):%d\n",y);
+}
+
+int main()
+{
+    int nbr=2;
+    int x=10;
+    int y=3;
+    
+    
+    printf("il valore inziale:%d\n", x);
+    ft_ft(&x,&nbr,y);
+    
+    
+    printf("il valore di:%d\n", x);
+    printf("il valore di:%d\n",y);
+    printf("il valore effettivo dopo aver richiamato ft_ft:%d\n", nbr);
+    
+
+    return 0;
+}
