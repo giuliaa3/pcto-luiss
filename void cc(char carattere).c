b708ac655acea6ab5a853a800cff6094192a4215
giuliaa3 <124038808+giuliaa3@users.noreply.github.com>
2023-02-02 16:57:03 +0100

    Add files via upload

---
+++ void cc(char carattere).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void cc(char carattere) {
+    printf("%c",carattere );
+}
+
+int main()
+{
+    char numero;
+    scanf("%c", &numero);
+cc(numero);
+    return 0;
+}
